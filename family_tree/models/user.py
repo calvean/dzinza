4852973bdcc1fff51ed49d33a970f1273fd8cf52
calvean <garnetsharara@gmail.com>
2023-12-09 16:21:40 +0200

    added user class

---
+++ family_tree/models/user.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+from models import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    role = db.Column(db.String(20), default='user')
