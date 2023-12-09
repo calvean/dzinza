7029bf970885d2e619629d815e9fe05993bc243b
calvean <garnetsharara@gmail.com>
2023-12-09 16:20:31 +0200

    created family tree class

---
+++ family_tree/models/family_tree.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+from models import db
+
+class FamilyTree(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship('User', backref=db.backref('family_trees', lazy=True))
